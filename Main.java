d5ea3658e45c64de05991fc6bdc1590d60bff438
Amit_C4 <casam@post.bgu.ac.il>
2024-08-20 02:44:26 +0300

    ab

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Game game = new Game();
         game.startGame();
+        // בראמי אם אתה רואה את זה אתה חייב לנו עוגת גבינה עם תותים
+        //ההערה הזאת נכתבה כשתכנתתי (עמית) שיכור סליחה מראש 
+        //נ.ב : השארתי את זה כי זה קומנט מצחיק
     }
 }
